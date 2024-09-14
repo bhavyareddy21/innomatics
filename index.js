b7d0b12d17a5c3994d82a55bf28c3d759b69fd6c
bhavyareddy21 <144778872+bhavyareddy21@users.noreply.github.com>
2024-09-14 16:28:58 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+index.js
+// src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom/client'; // Note the change here
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
